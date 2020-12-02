00c4263b38c27b8ab1888a5fbbdb77386d94d407
Diana Eddie <hello@dianaeddie.com>
2020-12-02 13:50:21 +0900

    Changed to final structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
 $(document).ready( function(){
+
+    //Hero image 
+    $(window).scroll(function () {
+        // Store the distance scrolled
+        var scrolled = $(window).scrollTop() + 1;
+    
+        // Set the scroll speed
+        var scrollSpeed = 0.3;
+        
+        // Update the background position
+        $(".hero-unit").css('background-position', '0' + -(scrolled * scrollSpeed) + 'px');
+    });
+
+    $(window).bind('scroll', update);
+
+
+    //Move to portfolio on click - from Hero Image
     $('#portfolio-button').click (function(){
         $('html, body').animate({
             scrollTop: $('#portfolio-section').offset().top
         }, 1500);
     });
+
+    
+
+   
 });
